aa2f7d29928f6545b8b0457a902a7b3817d07aad
Florian MARGAINE <florian@margaine.com>
2015-08-08 13:21:53 +0200

    Implements the /db/[table-name]/id file.

--- src/read-file.lisp
+++ src/read-file.lisp
@@ -4,4 +4,12 @@
   (:documentation "Reads a single file."))
 
 (defmethod read-file (path (type (eql :root)) &key)
-  "")
+  (when (table-exists (first path))
+    (read-file (rest path) :table :table (first path))))
+
+(defmethod read-file (path (type (eql :table)) &key table)
+  (let ((file (first path)))
+    (cond ((string= file "id") (read-file nil :table-id :table table)))))
+
+(defmethod read-file (path (type (eql :table-id)) &key table)
+  (format nil "~A~%" (table-primary-key table)))
--- src/table.lisp
+++ src/table.lisp
@@ -11,3 +11,19 @@ AND tablename = $1
 " table :single))
     (declare (ignore _))
     (> count 0)))
+
+(defn table-primary-key (string -> string) (table)
+  (multiple-value-bind (key _)
+      (with-db
+        (postmodern:query "
+SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS data_type
+FROM   pg_index i
+JOIN   pg_attribute a ON a.attrelid = i.indrelid
+                     AND a.attnum = ANY(i.indkey)
+WHERE  i.indrelid = $1::regclass
+AND    i.indisprimary
+" table :plist))
+    (declare (ignore _))
+    (if key
+        (getf key :attname)
+        "")))
